ca78dc69258c5ab733cc82e891c0536cbfbb69d7
tr43om <tr43om@gmail.com>
2023-02-09 21:57:41 +0600

    Now we can see day and month on home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { StripeCalendar } from "@/components";
 import { useScheduleStore } from "@/zustandStore";
+import { CurrentDate } from "@/components/CurrentDate";
 
 export default function Home() {
   return (
     <main>
       <StripeCalendar />
+      <CurrentDate />
     </main>
   );
 }
