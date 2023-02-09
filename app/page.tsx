75b7ebec8105f00a9b95b9748bebcd244f85c318
tr43om <tr43om@gmail.com>
2023-02-09 22:47:10 +0600

    Fetched schedule at main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,28 @@ import styles from "./page.module.css";
 import { StripeCalendar } from "@/components";
 import { useScheduleStore } from "@/zustandStore";
 import { CurrentDate } from "@/components/CurrentDate";
+import { Query, collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/firebase.config";
+
+const getSchedule = async () => {
+  const ref = query(
+    collection(db, `schedule/${"bAXxgsZgS37AQbMD0SSv"}/week_${1}`),
+    where("weekday", "==", "понедельник")
+  ) as Query<{}>;
+
+  const { docs } = await getDocs(ref);
+
+  const schedule = docs.map((schedule) => {
+    return { ...schedule.data(), id: schedule.id };
+  });
+
+  return schedule;
+};
 
 export default async function Home() {
+  const schedule = await getSchedule();
+
+  console.log(schedule);
   return (
     <main>
       <StripeCalendar />
