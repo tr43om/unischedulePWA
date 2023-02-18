32da3ee1b46c7769a9af3301e0c70b68ea2dcd35
tr43om <tr43om@gmail.com>
2023-02-18 17:23:18 +0600

    fixed error

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -3,85 +3,85 @@ import React from "react";
 import { OmsuGroupType, OmsuScheduleType } from "@/types";
 import { DataToFirebase } from "@/components/DataToFirebase";
 
-const getGroups = async () => {
-  const res = await fetch(
-    "https://eservice.omsu.ru/schedule/backend/dict/groups"
-  );
+// const getGroups = async () => {
+//   const res = await fetch(
+//     "https://eservice.omsu.ru/schedule/backend/dict/groups"
+//   );
 
-  const { data }: { data: OmsuGroupType[] } = await res.json();
+//   const { data }: { data: OmsuGroupType[] } = await res.json();
 
-  const groups = data.map((group) => {
-    if (group.name !== "Резерв") {
-      const year = Number(group.name.split("-")[1][0]);
-      const course = Number((22 - year).toString()[1]) + 1;
-      return { ...group, course };
-    }
+//   const groups = data.map((group) => {
+//     if (group.name !== "Резерв") {
+//       const year = Number(group.name.split("-")[1][0]);
+//       const course = Number((22 - year).toString()[1]) + 1;
+//       return { ...group, course };
+//     }
 
-    return group;
-  });
+//     return group;
+//   });
 
-  return groups;
-};
+//   return groups;
+// };
 
-const getSchedule = async (group: number) => {
-  try {
-    const res = await fetch(
-      `https://eservice.omsu.ru/schedule/backend/schedule/group/${group}`
-    );
+// const getSchedule = async (group: number) => {
+//   try {
+//     const res = await fetch(
+//       `https://eservice.omsu.ru/schedule/backend/schedule/group/${group}`
+//     );
 
-    if (!res.json) return;
+//     if (!res.json) return;
 
-    const { data }: { data: { day: string; lessons: OmsuScheduleType[] }[] } =
-      await res.json();
+//     const { data }: { data: { day: string; lessons: OmsuScheduleType[] }[] } =
+//       await res.json();
 
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
+//     return data;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-const getScheduleAll = async () => {
-  try {
-    const groupRes = await fetch(
-      "https://eservice.omsu.ru/schedule/backend/dict/groups"
-    );
+// const getScheduleAll = async () => {
+//   try {
+//     const groupRes = await fetch(
+//       "https://eservice.omsu.ru/schedule/backend/dict/groups"
+//     );
 
-    const { data: groups }: { data: OmsuGroupType[] } = await groupRes.json();
+//     const { data: groups }: { data: OmsuGroupType[] } = await groupRes.json();
 
-    const allSchedule = await Promise.all(
-      groups.map(async (group) => {
-        const res = await fetch(
-          `https://eservice.omsu.ru/schedule/backend/schedule/group/${group.id}`
-        );
+//     const allSchedule = await Promise.all(
+//       groups.map(async (group) => {
+//         const res = await fetch(
+//           `https://eservice.omsu.ru/schedule/backend/schedule/group/${group.id}`
+//         );
 
-        if (!res.ok) return;
+//         if (!res.ok) return;
 
-        const lessons = await res.json();
+//         const lessons = await res.json();
 
-        return { group: group.name, schedule: lessons };
-      })
-    );
+//         return { group: group.name, schedule: lessons };
+//       })
+//     );
 
-    return allSchedule;
-  } catch (err) {
-    console.log(err);
-  }
-};
+//     return allSchedule;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
 const Settings = async () => {
-  const groups = await getGroups();
-  const schedule = await getSchedule(1342);
-  const allSchedule = await getScheduleAll();
+  // const groups = await getGroups();
+  // const schedule = await getSchedule(1342);
+  // const allSchedule = await getScheduleAll();
 
-  console.table(allSchedule);
+  // console.table(allSchedule);
 
   return (
     <div>
-      <DataToFirebase
+      {/* <DataToFirebase
         schedule={schedule}
         groups={groups}
         allSchedule={result}
-      />
+      /> */}
     </div>
   );
 };
