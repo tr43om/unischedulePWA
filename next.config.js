6a43a8e4f471cc6115efdd6a4b0383f242e4f7c7
tr43om <tr43om@gmail.com>
2023-03-27 13:41:12 +0600

    pwa support

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const config = {
 };
 
 const nextConfig = require("@ducanh2912/next-pwa").default({
-  // dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
