43c1b3371fe914778ce8790009f3a139886d8751
tr43om <tr43om@gmail.com>
2023-03-31 16:59:00 +0600

    made changes to config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 // import withPWA from "next-pwa";
 
-const runtimeCaching = require("next-pwa/cache");
 const isDev = process.env.NODE_ENV !== "production";
 
 const config = {
@@ -13,24 +12,6 @@ const config = {
 const nextConfig = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: isDev,
-
-  exclude: [
-    // add buildExcludes here
-    ({ asset, compilation }) => {
-      if (
-        asset.name.startsWith("server/") ||
-        asset.name.match(
-          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
-        )
-      ) {
-        return true;
-      }
-      if (isDev && !asset.name.startsWith("static/runtime/")) {
-        return true;
-      }
-      return false;
-    },
-  ],
 })(config);
 
 module.exports = nextConfig;
