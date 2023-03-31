708efe544fa103e0b23fdc8bcc5a0c6ab6f52dc3
tr43om <tr43om@gmail.com>
2023-03-31 13:24:12 +0600

    🐓

--- app/(schedule)/layout.tsx
+++ app/(schedule)/layout.tsx
@@ -5,8 +5,6 @@ import {
   StripeCalendar,
   ViewToggle,
 } from "@/components";
-import { Suspense } from "react";
-import Loading from "../loading";
 
 export default function RootLayout({
   children,
